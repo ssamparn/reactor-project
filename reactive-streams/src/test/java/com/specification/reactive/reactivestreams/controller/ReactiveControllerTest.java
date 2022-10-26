cca37853bd464326cacd61cda71b47ae744647a3
samantrs <https://samantrs@git.rabobank.nl>
2022-10-26 21:32:45 +0200

    reactive controller test added

---
+++ reactive-streams/src/test/java/com/specification/reactive/reactivestreams/controller/ReactiveControllerTest.java
@@ -0,0 +1,105 @@
+package com.specification.reactive.reactivestreams.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DirtiesContext
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "10000") //By default, the WebTestClient is timedout after 5 seconds. We can configure the timeout with @AutoConfigureWebTestClient
+@ExtendWith(SpringExtension.class)
+public class ReactiveControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    final void get_simple_flux_test() {
+        Flux<Integer> integerFluxResponse = webTestClient.get()
+                .uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFluxResponse)
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .expectNext(5)
+                .expectNext(6)
+                .verifyComplete();
+    }
+
+    @Test
+    final void get_simple_flux_2_test() {
+
+        EntityExchangeResult<List<Integer>> entityExchangeResult = webTestClient.get()
+                .uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+        Assertions.assertEquals(entityExchangeResult.getResponseBody(), Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    final void get_simple_flux_3_test() {
+
+        webTestClient.get()
+                .uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .consumeWith(response -> Assertions.assertEquals(response.getResponseBody(), Arrays.asList(1, 2, 3, 4, 5, 6)));
+    }
+
+    @Test
+    final void flux_stream_test() {
+        Flux<Long> longFluxResponse = webTestClient.get()
+                .uri("/fluxstream")
+                .accept(MediaType.APPLICATION_STREAM_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(longFluxResponse)
+                .expectSubscription()
+                .expectNext(0L, 1L, 2L, 3L)
+                .thenCancel()
+                .verify();
+    }
+
+    @Test
+    final void mono_test() {
+        webTestClient.get()
+                .uri("/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith((response) -> {
+                    Assertions.assertEquals(response.getResponseBody(), Integer.valueOf(1));
+                });
+    }
+}
