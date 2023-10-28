f5521383cbdadf58bba32f5d543c028cb44260fa
samantrs <https://samantrs@git.rabobank.nl>
2023-10-28 13:51:09 +0200

    SwitchOnFirstOperator tests added

--- reactive-streams/src/test/java/com/specification/reactive/reactivestreams/operator/SwitchOnFirstOperator.java
+++ reactive-streams/src/test/java/com/specification/reactive/reactivestreams/operator/SwitchOnFirstOperator.java
@@ -13,6 +13,47 @@ import java.util.function.Function;
 @Slf4j
 public class SwitchOnFirstOperator {
 
+    /**
+     * switchOnFirst(): This Flux’s method helps us to switch flux pipeline based on the first emitted value. This method will receive 2 parameters.
+     *     1. First emitted signal
+     *     2. The Flux itself
+     *
+     * It should return the publisher. It could transform the pipeline if it is required.
+     * */
+
+    @Test
+    public void switch_on_operator_simple_test() {
+        Flux.just(1, 2, 3, 4, 5, 6, 7)
+                .defaultIfEmpty(100)
+                .subscribe(RsUtil.subscriber());
+        // This code will return 1 to 7 entirely. defaultIfEmpty block will not be kicked in.
+
+        Flux.just(1, 2, 3, 4, 5, 6, 7)
+                .switchOnFirst(((signal, integerFlux) -> {
+                    if (signal.get() == 2) {
+                        return integerFlux;
+                    } else return Flux.empty();
+                }))
+                .defaultIfEmpty(100)
+                .subscribe(RsUtil.subscriber());
+
+        // Here switchOnFirst() is added which checks if the first emitted value is 2. If it is 2, then we will receive other elements as well.
+        // If not, then I get the default value of 100 as the switchOnFirst cancels the source emission and publishes empty to downstream.
+
+        Flux.just(1, 2, 3, 4, 5, 6, 7)
+                .switchOnFirst(((signal, integerFlux) -> {
+                    if (signal.get() == 1) {
+                        return integerFlux;
+                    } else return Flux.empty();
+                }))
+                .defaultIfEmpty(100)
+                .subscribe(RsUtil.subscriber());
+
+        // Here switchOnFirst() is added which checks if the first emitted value is 1. If it is 1, then we will receive other elements as well.
+        // If not, then I get the default value of 100 as the switchOnFirst cancels the source emission and publishes empty to downstream.
+    }
+
+
     @Test
     public void switch_on_first_operator_test() {
         getPerson()
