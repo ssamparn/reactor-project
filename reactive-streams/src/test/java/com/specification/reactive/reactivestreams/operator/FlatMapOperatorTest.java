e77229d88e2b13909f686288fc5c3eb3ebbc5890
samantrs <https://samantrs@git.rabobank.nl>
2023-10-28 21:18:08 +0200

    flatMap documentation

--- reactive-streams/src/test/java/com/specification/reactive/reactivestreams/operator/FlatMapOperatorTest.java
+++ reactive-streams/src/test/java/com/specification/reactive/reactivestreams/operator/FlatMapOperatorTest.java
@@ -19,4 +19,15 @@ public class FlatMapOperatorTest {
                 .subscribe(RsUtil.subscriber());
         RsUtil.sleepSeconds(2);
     }
+
+    /* *
+     * Difference between map() and flatMap():
+     * The map operator applies a one-to-one transformation to stream elements, while flatMap does one-to-many.
+     * map(): Transform the items emitted by publisher by applying a synchronous function to each item.
+     * flatMap(): Transform the elements emitted by publisher asynchronously into Publishers.c
+     * It’s easy to see map is a synchronous operator – it’s simply a method that converts one value to another. This method executes in the same thread as the caller.
+     * The other statement – flatMap is asynchronous – is not that clear. In fact, the transformation of elements into Publishers can be either synchronous or asynchronous.
+     * In our sample code, that operation is synchronous since we emit elements with the Flux#just method. However, when dealing with a source that introduces high latency, such as a remote server, asynchronous processing is a better option.
+     * The important point is that the pipeline doesn’t care which threads the elements come from – it just pays attention to the publishers themselves.
+     */
 }
