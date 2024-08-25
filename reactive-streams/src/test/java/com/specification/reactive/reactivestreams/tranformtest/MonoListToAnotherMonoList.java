0c20b6a7a0d6ba6bc691eee5782e3956501081bb
Sashank <sashanka.samantray@rabobank.nl>
2024-08-25 13:41:31 +0200

    flatMapMany() and flatMapIterable()

--- reactive-streams/src/test/java/com/specification/reactive/reactivestreams/tranformtest/MonoListToAnotherMonoList.java
+++ reactive-streams/src/test/java/com/specification/reactive/reactivestreams/tranformtest/MonoListToAnotherMonoList.java
@@ -1,33 +1,45 @@
 package com.specification.reactive.reactivestreams.tranformtest;
 
 import com.specification.reactive.reactivestreams.model.MasterPair;
+import com.specification.reactive.reactivestreams.util.RsUtil;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
 import java.util.List;
 
+/* *
+ * We can use either flatMapMany() or flatMapIterable() to convert a Mono<List<T>> to a Flux<T>
+ * */
 public class MonoListToAnotherMonoList {
 
+    private static final List<MasterPair> masterPairs = List.of(
+            new MasterPair(1L),
+            new MasterPair(2L),
+            new MasterPair(3L)
+    );
+
     @Test
-    public void transform_monoOfList_ToAnotherMonoOfList_test() {
+    public void transform_monoOfList_ToAnotherMonoOfList_using_flatMapMany_test() {
+        Mono<List<MasterPair>> monoListMasterPair = Mono.fromSupplier(() -> masterPairs);
 
-        List<MasterPair> masterPairs = List.of(
-                new MasterPair(1L),
-                new MasterPair(2L),
-                new MasterPair(3L)
-        );
+        Mono<List<Long>> monoListLong = monoListMasterPair
+                .flatMapMany(Flux::fromIterable)
+                .map(MasterPair::getPrivateKey)
+                .collectList();
 
-        Mono<List<MasterPair>> monoListMasterPair = Mono.just(masterPairs);
+        monoListLong.subscribe(RsUtil.subscriber("flatMapMany() subscriber"));
+    }
+
+    @Test
+    public void transform_monoOfList_ToAnotherMonoOfList_using_flatMapIterable_test() {
+        Mono<List<MasterPair>> monoListMasterPair = Mono.fromSupplier(() -> masterPairs);
 
         Mono<List<Long>> monoListLong = monoListMasterPair
                 .flatMapIterable(pairs -> pairs)
                 .map(MasterPair::getPrivateKey)
                 .collectList();
 
-        StepVerifier.create(monoListLong)
-                .expectNextCount(1)
-                .verifyComplete();
-
+        monoListLong.subscribe(RsUtil.subscriber("flatMapIterable() subscriber"));
     }
 }
